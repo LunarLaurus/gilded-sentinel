a6070e81dcc1888f0c7cd38737d26ed8d99f6fae
LunarLaurus <draknyte1@gmail.com>
2024-09-02 13:33:09 +0100

    Actually inject env.js into primary index page. Log more during setup process.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Gilded-Sentinel Docker Entrypoint v0.0.1"
 # Ensure envsubst is available (Alpine-based images should have it installed by default)
 if ! command -v envsubst >/dev/null 2>&1; then
     echo "envsubst could not be found. Installing..."
@@ -7,7 +8,11 @@ if ! command -v envsubst >/dev/null 2>&1; then
 fi
 
 # Substitute environment variables into the env.js template
+echo "Substituting environmental variables into env.js"
 envsubst < /usr/share/nginx/html/env.js.template > /usr/share/nginx/html/env.js
+echo "env.js generated with content:"
+cat /usr/share/nginx/html/env.js
 
 # Start Nginx
+echo "Executing Nginx"
 exec nginx -g 'daemon off;'
