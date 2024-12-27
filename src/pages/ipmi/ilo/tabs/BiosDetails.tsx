59917ce23e54a7f19c89b7e8505caf1d2f9c2fa2
LunarLaurus <draknyte1@gmail.com>
2024-12-27 10:22:16 +0000

    Improve Ilo Bios information tab.

--- src/pages/ipmi/ilo/tabs/BiosDetails.tsx
+++ src/pages/ipmi/ilo/tabs/BiosDetails.tsx
@@ -2,17 +2,51 @@ import React from 'react';
 import { AuthenticatedClient } from '../../../../types/IloInterfaces';
 
 interface Props {
-    client?: AuthenticatedClient;
+    client: AuthenticatedClient;
 }
 
-const BiosDetails: React.FC<Props> = ({ client }) => (
-    <div>
-        <h2>Bios</h2>
-        <p>Current BIOS: {JSON.stringify(client?.bios?.current || 'N/A')}</p>
-        <p>Backup BIOS: {JSON.stringify(client?.bios?.backup || 'N/A')}</p>
-        <p>Bootblock BIOS: {JSON.stringify(client?.bios?.bootBlock || 'N/A')}</p>
-        <p>UEFI Class: {client?.bios?.uefiClass}</p>
-    </div>
-);
+const BiosDetails: React.FC<Props> = ({ client }) => {
+    const hasBootBlock = client.bios.bootBlock !== null;
+    const validBootBlock = hasBootBlock 
+        && client.bios.bootBlock.version !== null 
+        && client.bios.bootBlock.version.length > 0;
+
+    return (
+        <div className="ipmi-management-clients-grid">
+            <div
+                key={client.bios.current.version}
+                className="ipmi-management-client-card"
+            >
+                <h3>Current</h3>
+                <p>
+                    Date: {client.bios.current.date}<br />
+                    Family: {client.bios.current.family}<br />
+                </p>
+            </div>
+            <div
+                key={client.bios.backup.version}
+                className="ipmi-management-client-card"
+            >
+                <h3>Backup</h3>
+                <p>
+                    Date: {client.bios.backup.date}<br />
+                    Family: {client.bios.backup.family}<br />
+                </p>
+            </div>
+            {validBootBlock && (
+                <div
+                    key={client.bios.bootBlock.version}
+                    className="ipmi-management-client-card"
+                >
+                    <h3>Boot Block</h3>
+                    <p>
+                        Date: {client.bios.bootBlock.date}<br />
+                        Family: {client.bios.bootBlock.family}<br />
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default BiosDetails;
